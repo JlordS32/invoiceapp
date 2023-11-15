8b196b61de8038a5660221a23b4c0de23d9cb70f
JlordS32 <jlordsty128@gmail.com>
2023-11-15 22:01:29 +1100

    media queries

--- invoice-app/src/features/invoice/components/CreateInvoice.tsx
+++ invoice-app/src/features/invoice/components/CreateInvoice.tsx
@@ -24,10 +24,16 @@ interface CreateInvoiceProps {
 import { Link } from 'react-router-dom';
 import Status from './Status';
 
+import { useMediaQuery } from 'react-responsive';
+
 const CreateInvoice = ({ invoice }: CreateInvoiceProps) => {
 	const status: paymentStatusType = checkStatusType(invoice.status);
 
 	const total = getTotal(extractPrices(invoice));
+
+	const isWide = useMediaQuery({
+		query: '(min-width: 650px)',
+	});
 
 	return (
 		<Link to={`/invoice/${invoice.id}`}>
@@ -50,16 +56,20 @@ const CreateInvoice = ({ invoice }: CreateInvoiceProps) => {
 				<div className={styles.totalPrice}>
 					<div>{formatCurrency(total)}</div>
 				</div>
-				<Status status={status} />
-
-				<div className={styles.goTo}>
-					<div>
-						<img
-							src={arrowRight}
-							alt='arrow right'
-						/>
-					</div>
+				<div className={styles.statusWrapper}>
+					<Status status={status} />
 				</div>
+
+				{isWide && (
+					<div className={styles.goTo}>
+						<div>
+							<img
+								src={arrowRight}
+								alt='arrow right'
+							/>
+						</div>
+					</div>
+				)}
 			</div>
 		</Link>
 	);
